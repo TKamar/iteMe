2334fbc503e5d13a0179783909e3eae128e510d7
Tomer <ktomer5@gmail.com>
2022-06-27 15:09:36 +0200

    Add View Controllers

--- iteMe/AppDelegate.swift
+++ iteMe/AppDelegate.swift
@@ -1,11 +1,6 @@
-//
-//  AppDelegate.swift
-//  iteMe
-//
-//  Created by user216739 on 6/27/22.
-//
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
